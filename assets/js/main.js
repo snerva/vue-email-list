0ec01e425590ed30a002d2794b5a130d8d15b7de
snerva <selenenerva@live.it>
2022-11-08 15:28:48 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+const {createApp} = Vue 
+
+createApp({
+    data(){
+        return{
+
+        }
+    }
+}).mount('#app')
