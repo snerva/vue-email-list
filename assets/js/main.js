0e230a5bafa71b1f4087e5dd521c50a44eb438e9
snerva <selenenerva@live.it>
2022-11-08 15:56:03 +0100

    print email list

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,18 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-
+            emailAddList: []
         }
+    },
+    mounted(){
+        for (let i=0; i < 10; i++){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.emailAddList.push(response.data.response)  
+            })
+        }
+        console.log(this.emailAddList);
     }
 }).mount('#app')
